92221fdbfec9b093c08483b107e3dbc5366f2411
syunkitajima <syn.kitajima@gmail.com>
2023-12-23 11:59:55 +0900

    全ファイル修正

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
-require_relative '../lib/card'
-require_relative '../lib/player'
-require_relative '../lib/dealer'
+require_relative '../Quest/card'
+require_relative '../Quest/player'
+require_relative '../Quest/dealer'
 
 class Game
     def initialize
