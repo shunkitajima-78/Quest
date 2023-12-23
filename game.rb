a1892449af84c048656edf7d5c0c3c9d4577c11b
syunkitajima <syn.kitajima@gmail.com>
2023-12-23 10:29:34 +0900

    ゲーム処理の追加

--- game.rb
+++ game.rb
@@ -3,3 +3,20 @@ require_relative '../lib/player'
 require_relative '../lib/dealer'
 
 class Game
+    def start
+        puts "ブラックジャックを開始します。"
+        initialize
+        show_info
+        game_play
+        puts "ブラックジャックを終了します。"
+    end
+
+    #カード情報の開示
+    def show_info
+        @player.cards.each do |card|
+            show_player_info(card)
+        end
+
+        show_dealer_info(@dealer.cards[0])
+        puts "ディーラーの2枚目のカードは分かりません。"
+    end
