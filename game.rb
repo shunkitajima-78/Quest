c5088ff84af224a483707dff561c27d07d61bc6f
syunkitajima <syn.kitajima@gmail.com>
2023-12-23 10:34:33 +0900

    ゲーム処理の追加

--- game.rb
+++ game.rb
@@ -20,3 +20,46 @@ class Game
         show_dealer_info(@dealer.cards[0])
         puts "ディーラーの2枚目のカードは分かりません。"
     end
+
+    #ゲームの処理
+    def game_play
+        until @player.finished
+            player_hit_stand
+        end
+
+        if @player.bust
+            puts "#{@player.total}点でバーストしました。"
+        end
+
+        if @player.twenty_one
+            show_player_total_info
+        end
+
+        puts "ディーラーの2枚目のカードは#{@dealer.cards[1].card_info}"
+
+        until @dealer.finished
+            dealer_hit
+        end
+
+        show_dealer_total_info
+
+        if @dealer.bust
+            puts "#{@dealer.total}点でバーストしました。"
+        end
+
+        bust_judge_result
+
+    end
+
+    #プレイヤーのカード追加を判定
+    def player_hit_stand
+        puts "あなたの現在の得点は#{@player.total}です。カードを引きますか？（y/n）"
+        input = gets.chomp.downcase
+        if input == 'y'
+            card = Card.new(@cards.shift.send(:suit), @cards.shift.send(:number))
+            @player.hit(card)
+            show_player_info(card)
+        else
+            @player.stand!
+        end
+    end
